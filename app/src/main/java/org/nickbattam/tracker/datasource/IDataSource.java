e05d240babe49297fcd6f2411df8e3e3822abf9f
Nick Battam <nicholas.battam@tessella.com>
2017-05-21 19:09:01 +0100

    Code tidy

--- app/src/main/java/org/nickbattam/tracker/datasource/IDataSource.java
+++ app/src/main/java/org/nickbattam/tracker/datasource/IDataSource.java
@@ -4,10 +4,6 @@ import com.github.mikephil.charting.data.Entry;
 
 import java.util.List;
 
-/**
- * Created by nick on 21/05/17.
- */
-
 public interface IDataSource {
     List<Entry> getData();
 }
--- app/src/main/java/org/nickbattam/tracker/formatter/DateTimeAxisValueFormatter.java
+++ app/src/main/java/org/nickbattam/tracker/formatter/DateTimeAxisValueFormatter.java
@@ -11,11 +11,7 @@ import static org.nickbattam.tracker.datasource.Blood.BASE_DATETIME;
 
 public class DateTimeAxisValueFormatter implements IAxisValueFormatter {
 
-    private SimpleDateFormat mFormatter;
-
-    public DateTimeAxisValueFormatter() {
-        mFormatter = new SimpleDateFormat("hh:mm:ss");
-    }
+    private static SimpleDateFormat mFormatter = new SimpleDateFormat("dd MMM yyyy\nhh:mm:ss");
 
     @Override
     public String getFormattedValue(float value, AxisBase axis) {
